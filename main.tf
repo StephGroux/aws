15a6ebe9127d5273a776f7304fc1e925f5276476
Stephane Groux <stephane.groux@oracle.com>
2022-08-22 18:13:43 +0200

    remove security group

--- main.tf
+++ main.tf
@@ -117,18 +117,6 @@ resource "aws_lb_target_group" "asg" {
   }
 }
 
-resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
-
-
-  ingress {
-    from_port   = var.server_port
-    to_port     = var.server_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_security_group" "alb" {
   name = "terraform-example-alb"
 
