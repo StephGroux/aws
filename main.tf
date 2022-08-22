b14fd8035a8c1a05006d2d816dac7f8aaa64e811
Stephane Groux <stephane.groux@oracle.com>
2022-08-22 10:18:22 +0200

    ch2-asg

--- main.tf
+++ main.tf
@@ -4,34 +4,54 @@ variable "server_port" {
   default     = 8080
 }
 
-output "public_ip" {
-  value       = aws_instance.example.public_ip
-  description = "the public ip of the instance"
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 
 provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-052efd3df9dad4825"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+resource "aws_launch_configuration" "example" {
+  image_id      = "ami-052efd3df9dad4825"
+  instance_type = "t2.micro"
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
+  # Required when using a launch configuration with an auto scaling group.
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-  user_data_replace_on_change = true
-  tags = {
-    Name = "terraform-example"
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
 
 resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
+  name      = "terraform-example-instance"
+
 
   ingress {
     from_port   = var.server_port
