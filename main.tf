3fff8bb7fce5703d7a34343f6f8281ebea8e3c49
Stephane Groux <stephane.groux@oracle.com>
2022-08-22 09:23:14 +0200

    add vairables and output

--- main.tf
+++ main.tf
@@ -1,16 +1,27 @@
+variable "server_port" {
+  type        = number
+  description = "http port number"
+  default     = 8080
+}
+
+output "public_ip"{
+  value = aws_instance.example.public_ip
+  description = "the public ip of the instance"
+}
+
 provider "aws" {
   region = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-052efd3df9dad4825"
-  instance_type = "t2.micro"
+  ami                    = "ami-052efd3df9dad4825"
+  instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
   user_data_replace_on_change = true
@@ -23,8 +34,8 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
